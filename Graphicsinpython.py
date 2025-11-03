91ae42078330a5247e904f95f9a6c02ed1b95281
SHEMA IRAGENA Kevin <shemakevin320@gmail.com>
2025-11-03 19:53:59 +0000

    usng graphic features in python

---
+++ Graphicsinpython.py
@@ -0,0 +1,46 @@
+
+# import turtle
+# x = turtle.Turtle()
+# triangle = turtle.Screen()
+
+# triangle.bgcolor("white")
+# x.color("grey")
+
+# turtle.title("drawing some graphics using python")
+
+# turtle.up()
+# turtle.goto(50,250)
+# turtle.goto(250,250)
+# turtle.goto(25,250)
+# turtle.goto(50,50)
+
+# turtle.up()
+# # turtle.goto(150,50)
+# turtle.down()
+# turtle.goto(50,150)
+# turtle.tiltangle(100)
+
+# turtle.up()
+# turtle.goto(150,150)
+# turtle.write("it's kevin")
+
+# turtle.done()
+# input()
+
+def prime_number(lstpr,minum,Maximum):
+    for item in range(minum,Maximum+1):
+        if item >1 :
+            for i in range (2,int(item**0.5)+1):
+                if item % i == 0:
+                    break
+            else:    
+                lstpr.append(item)
+        
+        
+    return lstpr        
+lstpr = []
+minum = int(input("please enter the minumum number: "))
+maximum = int(input("please enter the maximun number: "))
+a = prime_number(lstpr,minum,maximum)
+
+print(a)
