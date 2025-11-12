5d69def2b872de3244c5f7787cbc4f2c4df5b79d
SHEMA IRAGENA Kevin <shemakevin320@gmail.com>
2025-11-12 10:05:26 +0000

    here you can now use the loop to add as many users as you want

--- creatingfile.py
+++ creatingfile.py
@@ -1,14 +1,14 @@
 # this is the way of creating and writing files in python 
 
 
-import re
+# import re
 
-users = {'alice': 'apassword', 'peter': 'ppassword', 'john': 'jpassword'}
+# users = {'alice': 'apassword', 'peter': 'ppassword', 'john': 'jpassword'}
 
-with open('users.txt', 'w') as file:
-    for username, password in users.items():
-        file.write(f"{username}:{password}\n")
-file.close()     
+# with open('users.txt', 'w') as file:
+#     for username, password in users.items():
+#         file.write(f"{username}:{password}\n")
+# file.close()     
 
 
 
@@ -22,3 +22,22 @@ john:jpassword
 
 
 '''
+
+
+
+import re
+
+users = {}
+numberofusers = int(input("how many users u want to add in to the file?: "))
+for i in range (numberofusers):
+    user = input("please enter your name ")
+    pasword = input("please enter your pasword")
+
+    users[user] = pasword
+
+with open('users.txt', 'w') as file:
+    for username, password in users.items():
+        file.write(f"{username}:{password}\n")
+file.close()     
+
+
